c4dd9c594a00e0711c6f8855084302260f4f1ae8
David <djshin94@gmail.com>
2017-11-30 21:50:42 -0800

    completed exercise

--- WebServer.java
+++ WebServer.java
@@ -1,22 +1,55 @@
-// ============================================================================
-// file: WebServer.java
-// ============================================================================
-// Programmer: David Shin
-// Date: 11/30/2017
-// Class: CS 380 ("Computer Networks")
-// Time: TR 3:00 - 4:50pm
-// Instructor: Mr. Davarpanah
-// Exercise 8
-//
-// Description: 
-//
-// ============================================================================
+/************************************************************************************
+ *	file: WebServer.java
+ *	author: Daniel Spencer, David Shin
+ *	class: CS 380 - computer networks
+ *
+ *	assignment: Exercise 7
+ *	date last modified: 11/30/2017
+ *
+ *	purpose: In this exercise, we will create a simple web server.
+ *
+ ************************************************************************************/
 
-public class WebServer
-{
-    public static void main(String[] args)
-    {
-	
+import java.net.*;
+import java.io.*;
+
+public class WebServer {
+    public static void main(String[] args) throws Exception {
+        try(ServerSocket sSocket = new ServerSocket(8080)) {
+            System.out.println("Server Socket Port 8080");
+            while(true) {
+                try(Socket socket = sSocket.accept()) {
+                    PrintStream out = new PrintStream(socket.getOutputStream(), true, "UTF-8");
+                    InputStreamReader isr = new InputStreamReader(socket.getInputStream(), "UTF-8");
+                    BufferedReader br = new BufferedReader(isr);
+                    String firstLine = br.readLine();
+                    System.out.println(firstLine);
+		    while(br.ready())
+			{
+			    System.out.println(br.readLine());
+			}
+                    String[] str = firstLine.split(" ");
+                    File inputFile = new File("." + str[1]);
+                    if(inputFile.isFile()) {
+                        BufferedReader file = new BufferedReader(new FileReader(inputFile));
+                        out.println("HTTP/1.1 200 OK");
+                        out.println("Content-type: text/html");
+                        out.println("Content-length: " + inputFile.length() + "\n");
+			while(file.ready())
+			    out.println(file.readLine());
+                    }
+                    else {
+                        File FileNotFound = new File("Notfound.html");
+                        BufferedReader NotFound = new BufferedReader(new FileReader(FileNotFound));
+                        out.println("HTTP/1.1 404 Not Found");
+                        out.println("Content-type: text/html");
+                        out.println("Content-length: " + FileNotFound.length() + "\n");
+			while(NotFound.ready())
+			    out.println(NotFound.readLine());
+
+                    }
+                }
+            }
+        }
     }
-    
 }
