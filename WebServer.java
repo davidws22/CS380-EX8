b7c3a47c8a8ee016ff6ad998488cfce78afe5f1a
David <djshin94@gmail.com>
2017-11-30 20:40:01 -0800

    setting up project

---
+++ WebServer.java
@@ -0,0 +1,22 @@
+// ============================================================================
+// file: WebServer.java
+// ============================================================================
+// Programmer: David Shin
+// Date: 11/30/2017
+// Class: CS 380 ("Computer Networks")
+// Time: TR 3:00 - 4:50pm
+// Instructor: Mr. Davarpanah
+// Exercise 8
+//
+// Description: 
+//
+// ============================================================================
+
+public class WebServer
+{
+    public static void main(String[] args)
+    {
+	
+    }
+    
+}
