a416da7e57bbb3b9d293af7cc4da90ac24bc26d6
Daniel Spencer <Daniel.Spencer87@gmail.com>
2017-11-30 21:30:33 -0800

    final

--- src/WebServer.java
+++ src/WebServer.java
@@ -24,22 +24,18 @@ public class WebServer {
                     BufferedReader br = new BufferedReader(isr);
                     String firstLine = br.readLine();
                     System.out.println(firstLine);
-                    while(br.ready()) {
-                        System.out.println(br.readLine());
-                    }
+                    System.out.println(br.readLine());
                     String[] str = firstLine.split(" ");
                     File inFile = new File("." + str[1]);
-                    if(inFile.exists() && inFile.isFile()) {
+                    if(inFile.isFile()) {
                         BufferedReader forFile = new BufferedReader(new FileReader(inFile));
                         out.println("HTTP/1.1 200 OK");
                         out.println("Content-type: text/html");
                         out.println("Content-length: " + inFile.length() + "\n");
-                        while(forFile.ready()) {
-                            out.println(forFile.readLine());
-                        }
+                        out.println(forFile.readLine());
                     }
                     else {
-                        File FileNotFound = new File("NotFound.html");
+                        File FileNotFound = new File("Notfound.html");
                         BufferedReader notFound = new BufferedReader(new FileReader(FileNotFound));
                         out.println("HTTP/1.1 404 Not Found");
                         out.println("Content-type: text/html");
